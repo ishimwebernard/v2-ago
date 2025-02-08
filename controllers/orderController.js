76aee1031b6171438ee616c2d48133f79e829507
ishimwebernard <isbernard2001@gmail.com>
2025-02-08 18:55:14 +0200

    mend

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -151,7 +151,7 @@ module.exports = {
       const order = await Order.findByPk(id, { include: OrderItem });
       if (!order) return res.status(404).json({ error: 'Order not found' });
 
-      
+
       res.json(order);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -160,7 +160,7 @@ module.exports = {
   async getOrdersByCustomer(req, res) {
     try{
       const { customerId } = req.params
-      const foundOrders = await Order.findAll({where: {customerId: customerId}})
+      const foundOrders = await Order.findAll({where: {customerId: customerId}, include: OrderItem})
       if (!foundOrders) return res.status(404).json({error: 'No such customer'})
       res.json(foundOrders)
     }catch(err){
